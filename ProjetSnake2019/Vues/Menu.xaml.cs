1fc21126d349275fa39be9bfc69592a64a00551b
Morel Alexandre - DIVTEC <alexandre.morel@divtec.ch>
2019-09-25 09:41:56 +0200

    Documentation

--- ProjetSnake2019/Vues/Menu.xaml.cs
+++ ProjetSnake2019/Vues/Menu.xaml.cs
@@ -1,35 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ProjetSnake2019
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Logique d'interaction pour Menu.xaml
     /// </summary>
     public partial class Menu : Window
     {
+        /// <summary>
+        /// Constructeur par défaut.
+        /// </summary>
         public Menu()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Clique sur le bouton "Jouer", lancement d'une partie.
+        /// </summary>
         private void BT_JOUER_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Clique sur le bouton "Quitter", fermeture de l'application.
+        /// </summary>
         private void BT_QUITTER_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
