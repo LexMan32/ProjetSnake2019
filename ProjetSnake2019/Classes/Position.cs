9cb58575949f56cd215cda2181a2edfc54c4294c
Morel Alexandre - DIVTEC <alexandre.morel@divtec.ch>
2019-09-25 11:22:12 +0200

    Déclaration classe Position

--- ProjetSnake2019/Classes/Position.cs
+++ ProjetSnake2019/Classes/Position.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjetSnake2019.Classes
+﻿namespace ProjetSnake2019.Classes
 {
-    class Position
+    public class Position
     {
+        private int x;
+        private int y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public void setY(int y)
+        {
+            this.y = y;
+        }
     }
 }
