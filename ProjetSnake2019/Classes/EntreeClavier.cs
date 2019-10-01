9e1202afc055e50e6a24be10ab42b064384bd0ad
Alexandre Morel - DIVTEC <alexandre.morel@divtec.ch>
2019-10-01 09:34:48 +0200

    Création d'une classe pour la gestion des entrées du clavier

---
+++ ProjetSnake2019/Classes/EntreeClavier.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Windows.Input;
+
+namespace ProjetSnake2019.Classes
+{
+    internal class EntreeClavier
+    {
+        // Liste des touches du clavier disponibles
+        private static Hashtable tableTouche = new Hashtable();
+
+        /// <summary>
+        /// Verifie si une touche du clavier spécifique à été appuyée.
+        /// </summary>
+        public static bool estTouchePressee(Key touche)
+        {
+            if (tableTouche[touche] == null)
+            {
+                return false;
+            }
+
+            return (bool)tableTouche[touche];
+        }
+
+        /// <summary>
+        /// Détecte si une touche du clavier est pressée.
+        /// </summary>
+        public static void ChangerEtat(Key touche, bool etat)
+        {
+            tableTouche[touche] = etat;
+        }
+
+    }
+}
